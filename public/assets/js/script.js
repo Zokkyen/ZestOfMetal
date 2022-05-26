3d70c07d640d1ea61aa5784de25cde11598abc16
Zokkyen <stephane.swaenepoel@wanadoo.fr>
2022-05-26 23:03:49 +0200

    Btn page Connexion à modifier

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,17 +7,25 @@ signupIn.forEach(item => {
         document.querySelector(".form-signup").classList.toggle("form-signup-left");
         document.querySelector(".signup-inactive").classList.toggle("signup-active");
         document.querySelector(".signin-active").classList.toggle("signin-inactive");
+        //document.querySelector(".signup-inactive").classList.add("signup-active").classList.remove(".signup-inactive");
+        //document.querySelector(".signin-active").classList.add("signin-inactive").classList.remove(".signup-active");
         document.querySelector(".forgot").classList.toggle("forgot-left");
         this.classList.remove(".idle").classList.add("active");
     });
 });
 
-const btnSignup = document.querySelectorAll('.btn-signup');
-btnSignup.addEventListener('click', function() {
-    document.querySelector(".form-signup-left").classList.toggle("form-signup-down");
-});
+/* Check du même mot de passe dans la partie inscription */
 
-const btnSignin = document.querySelectorAll('.btn-signin');
-btnSignin.addEventListener('click', function() {
-    document.querySelector(".forgot").classList.toggle("forgot-fade");
+const btnSignup = document.querySelector('.btn-signup');
+btnSignup.addEventListener('click', function() {
+    if((pswCheck1.value.localeCompare(pswCheck2.value)) == -1) {
+
+
+        pswCheck1.style.border = "1px solid red";
+        pswCheck2.style.border = "1px solid red";
+        
+
+
+
+    }
 });
