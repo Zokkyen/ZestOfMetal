764c47cd01da8b4bd63f0a24f1fce12c047949e3
Zokkyen <stephane.swaenepoel@wanadoo.fr>
2022-07-11 00:21:00 +0200

    admin dashboard try

--- view/admin.php
+++ view/admin.php
@@ -1,4 +1,3 @@
-
 <!-- Corps de la page -->
 <main>
     <div class="dashboard-fullContainer">
@@ -7,27 +6,85 @@
             <!-- Dashboard Title -->
             <div class="dashboard-title">
                 <?php
-                    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-                        echo "<span>Bienvenue, cher Admin " . htmlspecialchars($_SESSION['username']) . " !</span>";
-                    } else {
-                        echo "<span>Il y a un problème par ici !</span>";
-                    }
+                if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+                    echo "<span>Bienvenue, cher admin " . htmlspecialchars($_SESSION['username']) . " !</span>";
+                } else {
+                    echo "<span>Il y a un problème par ici !</span>";
+                }
                 ?>
-            </div>           
-            
+            </div>
+
             <!-- Dashboard Menu Navigation -->
             <div class="dashboard-menu">
-                <input type="submit" class="btn btn-secondary" name="infoDash" value="Informations">
-                <input type="submit" class="btn btn-secondary" name="artDash" value="Articles">
-                <input type="submit" class="btn btn-secondary" name="conDash" value="Concerts">
-                <input type="submit" class="btn btn-secondary" name="forDash" value="Forum">
-                <input type="submit" class="btn btn-secondary" name="memDash" value="Nvx membres">
-                <input type="submit" class="btn btn-secondary" name="appDash" value="Approbations">
+                <input type="submit" class="btn" name="userDash" value="Utilisateurs">
+                <input type="submit" class="btn" name="addDash" value="Ajout contenu">
+                <input type="submit" class="btn" name="approDash" value="Approbations">
             </div>
 
             <!-- Dashboard -->
-            <div class="dashboard-window"> 
-                <div class="dashboard">
+            <div class="dashboard-window">
+                <div class="dashboard-main">
+                    <h5 class="dashboard-subTitle m-0">Utilisateurs</h5>
+
+                    <div class="dashboard-content">
+
+                        <div>Row 1</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+
+                        <div>Row 2</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+
+                        <div>Row 3</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+
+                        <div>Row 4 too long for the first column</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+
+                        <div>Row 5</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+
+                        <div>Row 6</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+
+                        <div>Row 7</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>1.00</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                        <div>Description</div>
+                    </div>
+
 
                 </div>
 
@@ -40,7 +97,7 @@
 
 
 
-            </div>       
+            </div>
         </div>
     </div>
 </main>
