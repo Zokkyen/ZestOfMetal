8e950b1a616dce35319d8ebd78b3bc1a3197fafa
Zokkyen <stephane.swaenepoel@wanadoo.fr>
2022-07-20 21:26:14 +0200

    footer mobile + desktop ok

--- view/footer.php
+++ view/footer.php
@@ -1,47 +1,47 @@
 
     <!-- Partie Footer -->
     <footer>
-        <div class="container-fluid footer">
+        <div class="container-fluid footer sticky-bottom">
             <div class="row h-100">
 
-                <div class="col-6 col-md-4 text-center d-flex flex-column justify-content-center w-50 h-100 p-1">
-                    <div class="row h-100 align-items-center">
-                        <div class="col-12 col-md-3">
-                            <p class="m-1">&#169;Zokkyen</p>
+                <div class="col-6 col-md-7 text-center d-flex justify-content-center align-items-center p-1">
+                    <div class="row align-items-center justify-content-center w-100">
+                        <div class="col-12 col-md-3 col-lg-4">
+                            <p class="m-0 p-1">&#169;Zokkyen</p>
                         </div>
 
-                        <div class="col-12 col-md-3">
-                            <p class="m-1">Tous droits réservés.</p>                        
+                        <div class="col-12 col-md-5 col-lg-4">
+                            <p class="m-0 p-1">Tous droits réservés.</p>                        
                         </div>
 
-                        <div class="col-12 col-md-6 d-flex justify-content-around">
+                        <div class="col-12 col-md-4 col-lg-4 d-flex justify-content-evenly">
                             <a href="../public/assets/php/help.php">Aide</a>
                             <a href="../public/assets/php/cgu.php">CGU</a>                  
                         </div>
                     </div>
                 </div>
 
-                <div class="col-6 col-md-8 text-center d-flex flex-column justify-content-center w-50 h-100 p-1">
-                    <div class="row h-100 align-items-center">
-                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center h-50 p-1">
+                <div class="col-6 col-md-5 text-center d-flex justify-content-center align-items-center p-1">
+                    <div class="row align-items-center justify-content-center h-100 w-100">
+                        <div class="col-6 col-md-3 col-md-3 col-lg-2 p-0">
                             <a href="https://www.facebook.com/zestofmetal" target="_blank">
                                 <img src="../public/assets/img/ICO/facebook.svg" alt="facebook">
                             </a>
                         </div>
 
-                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center h-50 p-1">
+                        <div class="col-6 col-md-3 col-lg-2 p-0">
                             <a href="https://www.instagram.com/zestofmetal" target="_blank">
                                 <img src="../public/assets/img/ICO/instagram.svg" alt="instagram">
                             </a>
                         </div>
 
-                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center h-50 p-1">
+                        <div class="col-6 col-md-3 col-lg-2 p-0">
                             <a href="https://twitter.com/zestofmetal" target="_blank">
                                 <img src="../public/assets/img/ICO/twitter.svg" alt="twitter">
                             </a>
                         </div>
 
-                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center h-50 p-1">
+                        <div class="col-6 col-md-3 col-lg-2 p-0">
                             <a href="mailto:zestofmetal@contact.fr" target="_blank">
                                 <img src="../public/assets/img/ICO/mail.svg" alt="mail">
                             </a>
